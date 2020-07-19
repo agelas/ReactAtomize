8235c754f3d15937bc8142209951d1e8d90be0b3
Mathias <mathiaswi177@gmail.com>
2020-07-18 22:37:14 -0700

    Added background image somehow, added buttons, links need to be placed later

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 import {Provider as StyletronProvider, DebugEngine} from 'styletron-react';
 import {Client as Styletron} from 'styletron-engine-atomic';
 import {Div, Button, Icon, Text, Row, Col, Container, StyleReset, ThemeProvider} from 'atomize';
+import bubbleground from './double-bubble-outline/double-bubble-outline.png'
 
 const debug = process.env.NODE_ENV === "production" ? void 0 : new DebugEngine();
 //Create a client engine instance
@@ -22,22 +23,18 @@ class App extends Component {
       <StyletronProvider value = {engine} debug={debug} debugAfterHydration>
         <ThemeProvider theme={theme}>
           <StyleReset />
-          <Div 
-            minH = "8rem"
-            bg = "success100"
-          >
           
-          </Div>
           <Div
             textColor = "black900"
-            minH="50vh"
+            minH="35vh"
             w="100vw"
             d="flex"
             flexDir="column"
             align="center"
             textSize="display3"
-            p={{x:"1rem", y: "4rem"}}
-            bg = "success100"
+            p={{x:"1rem", y: "10rem"}}
+            bgImg = {bubbleground}
+            bgSize = "contain"
           >
             Mathias Insley
             <Text
@@ -51,38 +48,29 @@ class App extends Component {
             Student at Johns Hopkins University. Majoring in Materials Science & Engineering,
             minoring in Computer Science.
             </Text>
-            <Button
-              h="2.5rem"
-              w="2.5rem"
-              bg="warning700"
-              hoverBg="warning600"
-              rounded="circle"
-              m={{r:"1rem"}}
-              shadow="2"
-              hoverShadow="4"
-            >
-              <Icon name="BookmarkSolid" size = "20px" color = "white" />
-            </Button>
           </Div>
           <Container
             w="100rem"
             d="flex"
             flexDir="column"
             align="center"
-            bg = "black"
+            bg = "sucess100"
             minH = "10rem"
           >
           {
             <Row d = "flex" flexDir = "row" justify = "space-between">
-                <Col size="4" bg = "brand700" p="4rem">
+                <Col size="4" bg = "sucess100" p="4rem">
                   <Div 
                     d="flex"
                     p="1rem" 
-                    bg = "warning500" 
+                    bg = "success100" 
                     rounded = "md" 
                     minW = "8rem" 
                     minH = "10rem"
                     align="center"
+                    shadow="5"
+                    border="3px solid"
+                    borderColor="gray400"
                   >
                     <Button
                     prefix={
@@ -103,15 +91,18 @@ class App extends Component {
                   </Button> 
                   </Div>
                 </Col>
-                <Col size="4" bg = "brand700" p="4rem">
+                <Col size="4" bg = "sucess100" p="4rem">
                   <Div 
                     d = "flex"
                     p="1rem" 
-                    bg = "warning500" 
+                    bg = "success100" 
                     rounded = "md" 
                     minW = "8rem" 
                     minH = "10rem"
                     align="center"
+                    shadow="5"
+                    border="3px solid"
+                    borderColor="gray400"
                   >
                     <Button
                     prefix={
@@ -132,15 +123,18 @@ class App extends Component {
                     </Button> 
                   </Div>
                 </Col>
-                <Col size="4" bg = "brand700" p="4rem">
+                <Col size="4" bg = "sucess100" p="4rem">
                   <Div 
                     d = "flex"
                     p="1rem"
-                    bg = "warning500" 
+                    bg = "sucess100" 
                     rounded = "md" 
                     minW = "8rem" 
                     minH = "10rem" 
                     align = "center"
+                    shadow="5"
+                    border="3px solid"
+                    borderColor="gray400"
                   >
                     <Button
                       prefix={
